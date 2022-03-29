e58307a9b75818126f0445e2fa7a5893d1d002a5
Oscar Wystråle <osay21@student.bth.se>
2022-03-29 12:41:56 +0100

    Finished exercises for kmom01

---
+++ components/Stock.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import { Text, View } from 'react-native';
+import config from "../config/config.json";
+
+function StockList() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch(`${config.base_url}/products?api_key=${config.api_key}`)
+      .then(response => response.json())
+      .then(result => setProducts(result.data));
+  }, []);
+
+  const list = products.map((product, index) => <Text key={index}>{ product.name } - { product.stock }</Text>);
+
+  return (
+    <View>
+      {list}
+    </View>
+  );
+}
